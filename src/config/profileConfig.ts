02985427858780355fadb22e4d475f6c8c75c25b
Thebugisme <2874258529@qq.com>
2026-09-11 15:56:29 +0800

    bio

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -12,7 +12,7 @@ export const profileConfig: ProfileConfig = {
 	name: "Thebvg",
 
 	// 个人签名
-	bio: "",
+	bio: "花有重开日，人无再少年",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
